d1cf51d0f08aace5fde75fc8f280ce3cd7d0ec75
Gabriel Fiamoncini <gfiamoncini@unidavi.edu.br>
2018-09-13 20:50:09 -0300

    ..

--- php/src/gabrielfiamoncini/Aula - 13092018/PHP/index.php
+++ php/src/gabrielfiamoncini/Aula - 13092018/PHP/index.php
@@ -1,10 +1,18 @@
-<?php
-   $linha = 0;
-
-   while($linha < 50){
-       $linha++;
-       
-     echo"Teste de Impressão Linha Numero: ".$linha;
-     echo"<br>";
-   }
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+    <h1>Gabriel Fiamoncini</h1>
+    <div>
+      <div class="header">Cabeçalho</div>
+      <div class="menu">Menu</div>
+      <div class="principal">Páginas</div>
+      <div class="rodape">Rodapé</div>
+    </div>
+</body>
+</html>
