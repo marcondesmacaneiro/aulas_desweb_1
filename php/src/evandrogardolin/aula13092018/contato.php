87dc1900279b948f84ef1e68c715fc33951890e9
unidavi <evandrogardolinn@gmail.com>
2018-09-13 21:38:57 -0300

    Commit Final

---
+++ php/src/evandrogardolin/aula13092018/contato.php
@@ -0,0 +1,16 @@
+Entre em Contato
+evandro@unidavi.edu.br
+
+<hr>
+<?php
+    if ($_POST["enviar"] == "Enviar"){
+        echo "O nome é {$_POST[nome]}";
+    }
+?>
+<hr>
+<form method="POST">
+    <label for="nome">Nome</label>
+    <input type="text" name="nome" id="nome" value="<?php echo $_POST[nome];?>">
+    <br/>
+    <input type="submit" name="enviar" value="Enviar">
+</form>
