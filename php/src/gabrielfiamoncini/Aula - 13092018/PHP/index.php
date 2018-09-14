47a2a640d4cd5b42b05639867609c4cc9cae0b1d
Gabriel Fiamoncini <gfiamoncini@unidavi.edu.br>
2018-09-13 21:10:56 -0300

    ..

--- php/src/gabrielfiamoncini/Aula - 13092018/PHP/index.php
+++ php/src/gabrielfiamoncini/Aula - 13092018/PHP/index.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
-    <h1>Gabriel Fiamoncini</h1>
-    <div>
-      <div class="header">Cabeçalho</div>
-      <div class="menu">Menu</div>
-      <div class="principal">Páginas</div>
-      <div class="rodape">Rodapé</div>
-    </div>
-</body>
-</html>
+<?php
+    include "cabecalho.php";
+    include "home.php";
+    require "rodape.php";
+?>
