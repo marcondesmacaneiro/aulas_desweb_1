d54b2323f9cebb19a549f4ce0e41839f1207a053
Luiz Augusto Moratelli <gameplayteam12@gmail.com>
2018-09-13 21:43:33 -0300

    Exercicio

---
+++ php/src/luizmoratelli/Cabecalho.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="?url=home">Home</a></li>
+                <li><a href="?url=sobre">Sobre</a></li>
+                <li><a href="?url=contato">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
