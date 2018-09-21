eb7954c82585bfdb07f42aa98d6886a464c9b944
“Hugo <hugohoffmann04@gmail.com>
2018-09-20 22:07:40 -0300

    Arquivos de login adicionados

--- php/src/hugo/aula20-09-2018/exercício/index.php
+++ php/src/hugo/aula20-09-2018/exercício/index.php
@@ -1,5 +1,14 @@
 <pre>
 <?php
-    print_r($_SERVER)
+ //   print_r($_SERVER)
+    //Escrita
+    //setcookie($name, $value, $unixTime, $path)
+    //Leitura
+    //$_COOKIE[$nome]
+    if ($_GET["gravar"]){
+        setcookie("meu-cookie", "valor", time() + 86400, "/");
+        echo "cookie gravado";
+    }
+    print_r($_COOKIE)
 ?>
 </pre>
---
+++ php/src/hugo/aula20-09-2018/exercício/login/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="open.php" method="POST">
+    <label for="usuario"><b>Usuário</b></label>
+    <input type="text" placeholder="Insira um nome" name="usuario" required>
+    <br>
+
+    <label for="senha"><b>Senha</b></label>
+    <input type="password" placeholder="Insira uma senha" name="senha" required>
+    <br>
+    <button type="submit">Entrar</button>
+    </form>
+</body>
+</html>
