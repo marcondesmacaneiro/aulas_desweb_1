7ea7e12099b48f58e49fdd0de974ee3c234faf01
Unknown <viniciusstk@unidavi.edu.br>
2018-09-20 21:43:13 -0300

    Auto stash before merge of "master" and "origin/master"
    20_09

--- php/src/vinicius_stramosk/php/contato.php
+++ php/src/vinicius_stramosk/php/contato.php
@@ -1,17 +1,16 @@
 Contato:
 dale dele dele doly
 <hr>
+
 <?php
     if($_POST["enviar"] == "Enivar"){
         echo "O nome é {$_POST[nome]}.";
     }
+
 ?>
-<hr>
 <form method="POST">
     <label for="nome">Nome</label>
     <input type="text" name="nome" id="nome"
     value="<?php echo $_POST[nome]; ?>">
-    <br />
-
     <input type="submit" name="enviar" value="Enviar">
 </form>
