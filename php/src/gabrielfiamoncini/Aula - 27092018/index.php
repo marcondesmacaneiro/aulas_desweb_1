0db43bfbd9598389f1b78c77b31e7bdd4c14eea1
Gabriel Fiamoncini <gfiamoncini@unidavi.edu.br>
2018-10-02 07:50:37 -0300

    ..

--- php/src/gabrielfiamoncini/Aula - 27092018/index.php
+++ php/src/gabrielfiamoncini/Aula - 27092018/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("127.0.0.1", "root", "", "web1");
+$conn = mysqli_connect("mysql", "root", "root", "web1");
     if (mysqli_connect_error()) {
         echo 'erro: '.mysql_connect_error();
         die();
@@ -10,7 +10,7 @@ $conn = mysqli_connect("127.0.0.1", "root", "", "web1");
     $query = mysqli_query($conn, $sql);
 
     while ($linha = mysqli_fetch_array($query)) {
-        echo $linha['primeiro_nome'].'<br>';
+        echo  $linha['primeiro_nome'].'<br>';
         }
             
 
