8d1d434fe329e47a61557423e91edc48199f928d
Marcondes Maçaneiro <marcondesmacaneiro@gmail.com>
2018-10-04 17:19:24 -0300

    Apenas para demonstração

--- php/src/marcondes/banco/index.php
+++ php/src/marcondes/banco/index.php
@@ -10,7 +10,5 @@ $query = "select * from pessoa";
 $result = mysqli_query($conn, $query);
 while ($linha = mysqli_fetch_array($result)) {
     echo "Linha: {$linha[primeiro_nome]}<br>";
-    flush();
-    sleep(5);
 }
 
