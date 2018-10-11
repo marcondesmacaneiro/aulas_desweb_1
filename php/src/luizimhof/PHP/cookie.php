7bfac42f30f8c7a99f6a2b59c091abf9918b8913
vinicius <viniciusstk@unidavi.edu.br>
2018-10-11 19:34:11 -0300

    correçao

--- php/src/luizimhof/PHP/cookie.php
+++ php/src/luizimhof/PHP/cookie.php
@@ -2,8 +2,8 @@
 
 <?php 
     if($_GET["gravar"]){
-        setcookie("meu-cookie", "valor", time()+86400, "/")
-        echo("cookie gravado")
+        setcookie("meu-cookie", "valor", time()+86400, "/");
+        echo("cookie gravado");
     }
     print_r($_COOKIE)
 ?>
