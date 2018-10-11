6f5510383dcf31894c9ddcfb04fb8d50059efd8a
unidavi <evandrogardolinn@gmail.com>
2018-10-11 20:35:47 -0300

    Codigo

--- php/src/evandrogardolin/aula2709/index.php
+++ php/src/evandrogardolin/aula2709/index.php
@@ -1,16 +1,32 @@
 <?php
-    $conn = mysqli_connect('mysql', 'root', 'root', 'web1');
-    if (mysqli_connect_error()){
-        echo "erro: ". mysqli_connect_error();
-    }
+    
+    include "includes/conexao.php";
+
+    // $query = "select * from pessoa";
+    // $result = mysqli_query($conn, $query);
+
+    // while ($linha = mysqli_fetch_array($result)){
+    //     echo "Linha {$linha[primeiro_nome]}<br/>";
+    //     flush();
+    //     sleep(3);
+    // }
 
 
-    $query = "select * from pessoa";
-    $result = mysqli_query($conn, $query);
+?>
 
-    while ($linha = mysqli_fetch_array($result)){
-        echo "Linha {$linha[primeiro_nome]}<br/>";
-        flush();
-        sleep(3);
+<form method="post">
+    PRIMEIRO NOME: <BR>
+    <input type="text" name="primeiro_nome">
+    <br>
+    <input type="submit" name="gravar" value="Gravar">
+</form>
+
+<?php
+    var_dump($_POST);
+
+    if (!isset($_POST["gravar"])){
+        echo "o registro nao foi gravado";
+    } else {
+        echo "o registro foi gravado";
     }
 ?>
