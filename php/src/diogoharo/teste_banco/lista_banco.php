35ec6022e3a026fecc7a45ed2fc7fc8430d3ddd5
vinicius stramosk <viniciusstk@unidavi.edu.br>
2018-10-11 21:27:01 -0300

    Aula 11/10/2018

--- php/src/diogoharo/teste_banco/lista_banco.php
+++ php/src/diogoharo/teste_banco/lista_banco.php
@@ -1,13 +1,35 @@
 <?php 
-    $conn = mysqli_connect('mysql', 'root', 'root' ,'web1');
-    if(mysqli_connect_error()) {
-        echo  'erro:' . mysqli_connect_error();
-        die();
-    }
+    require "../conexao.php";
 
     $sSql = "SELECT * FROM pessoa";
     $sQuery = mysqli_query($conn,$sSql);
 
-    while($linha= mysqli_fetch_array($sQuery)) {
-        echo $linha[primeiro_nome] . '<br/>';
-    }
+   
+?>
+    <table border="1">
+        <thead>
+            <th>ID</th>
+            <th>Primeiro Nome</th>
+            <th>Segundo Nome</th>
+            <th>E-Mail</th>
+            <th>Cidade</th>
+            <th>Estado</th>
+        </thead>
+        <tbody>
+            <?php
+             while($linha= mysqli_fetch_array($sQuery)) {
+                ?>
+            <tr>
+                <td><?=$linha['id']?></td>
+                <td><?=$linha['primeiro_nome']?></td>
+                <td><?=$linha['segundo_nome']?></td>
+                <td><?=$linha['email']?></td>
+                <td><?=$linha['cidade']?></td>
+                <td><?=$linha['estado']?></td>
+                
+            </tr>
+            <?php
+            }
+            ?>
+        </tbody>
+    </table>
