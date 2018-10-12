f4c560072e80f2e503917ab3acbd2643ae3ef889
Gabriel Fiamoncini <gfiamoncini@unidavi.edu.br>
2018-10-11 21:23:22 -0300

    ..

--- php/src/gabrielfiamoncini/Aula - 10102018/cadastro.php
+++ php/src/gabrielfiamoncini/Aula - 10102018/cadastro.php
@@ -1,11 +1,18 @@
 <?php
     //var_dump($_POST);
+    include "conexao.php";
 
     if(isset($_POST["gravar"])){
        //echo"Gravar o Registro";
         $nome=$_POST["primeiro_nome"];
 
-        echo $nome;
+        //echo $nome;
+        $sql = "insert into pessoa (primeiro_nome) values ('{$nome}')";
+        if(mysqli_query($conn,$sql)){
+            echo"Sucesso";
+        }else{
+            echo"Erro";
+        }
     }
 ?>
 
--- php/src/gabrielfiamoncini/Aula - 10102018/index.php
+++ php/src/gabrielfiamoncini/Aula - 10102018/index.php
@@ -2,9 +2,31 @@
     include "conexao.php";
     $sql = 'select * from pessoa';
     $query = mysqli_query($conn, $sql);
-
-    while ($linha = mysqli_fetch_array($query)) {
-        echo  $linha['primeiro_nome'].'<br>';
-        }       
-
 ?>
+<table border="1">
+    <tr>
+        <td>ID</td>
+        <td>Primeiro Nome</td>
+        <td>Segundo Nome</td>
+        <td>E-mail</td>
+        <td>Cidade</td>
+        <td>Estado</td>
+        <td>Ações</td>
+    </tr>
+    <?php
+        while ($linha = mysqli_fetch_array($query)) {
+            //echo  $linha['primeiro_nome'].'<br>';
+        ?>
+         <tr>
+             <td><?=$linha["id"]?></td>
+             <td><?=$linha["primeiro_nome"]?></td>
+            <td><?=$linha["segundo_nome"]?></td>
+            <td><?=$linha["email"]?></td>
+            <td><?=$linha["cidade"]?></td>
+            <td><?=$linha["estado"]?></td>
+            <td>Ações</td>
+        </tr>            
+        <?php
+        }
+    ?>
+</table>
