3f78f3fcacb9cc8e988929891c93415925680b1b
Andre Lino <andre.cesar97@hotmail.com>
2018-10-18 19:03:52 -0300

    cadastro2

--- php/src/andre/data/cadastro.php
+++ php/src/andre/data/cadastro.php
@@ -7,8 +7,12 @@
     if(isset($_POST["gravar"])) {
         //echo "gravar o registro";
         $nome = $_POST["primeiro_nome"];
+        $sobrenome = $_POST["primeiro_nome"];
+        $cidade = $_POST["cidade"];
+        $estado = $_POST["estado"];
+        $email = $_POST["email"];
 
-        $query = "insert into pessoa (primeiro_nome) values ('{$nome}')";
+        $query = "insert into pessoa (primeiro_nome) values ('{$nome}','{$sobrenome}','{$cidade}','{$estado}','{$email}')";
 
         if(mysqli_query($conn, $query)) {
             echo "Registro gravado com sucesso";
@@ -28,6 +32,22 @@
     Primeiro nome:
     <br>
     <input type="text" name="primeiro_nome">
+    <br>
+    Segundo nome:
+    <br>
+    <input type="text" name="segundo_nome">
+    <br>
+    Cidade:
+    <br>
+    <input type="text" name="cidade">
+    <br>
+    Estado:
+    <br>
+    <input type="text" name="estado">
+    <br>
+    Email:
+    <br>
+    <input type="text" name="email">
     <br>
     <input type="submit" name="gravar" value="Gravar">
 
