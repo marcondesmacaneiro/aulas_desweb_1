bbcb688aa06db3b3f0f980638552c1a5e4d565b9
“Hugo <hugohoffmann04@gmail.com>
2018-10-25 23:00:50 -0300

    Adicionado arquivos do sistema, folha de estilo, conexão com o banco PostgreSQL

--- php/src/hugo/sistema_locacao/database.sql
+++ php/src/hugo/sistema_locacao/database.sql
@@ -4,22 +4,32 @@ CREATE TABLE tbcliente(
     cli_id SERIAL,
     cli_nome VARCHAR(30) NOT NULL,
     cli_documento VARCHAR(20) NOT NULL,
+    
     CONSTRAINT pk_cli_id PRIMARY KEY (cli_id),
 
 
-)
+);
 
 CREATE TABLE tbcarro(
     car_id SERIAL,
-    car_valor_diaria DECIMAL(10,2),
+    car_valor_diaria DECIMAL(10,2) NOT NULL,
+    car_modelo VARCHAR(25) NOT NULL,
+    car_ano INTEGER NOT NULL,
+    car_
 
-)
+);
 
 CREATE TABLE tbaluguel(
     alu_id SERIAL,
     cli_id INTEGER NOT NULL,
     car_id INTEGER NOT NULL,
+    alu_qtd_dia INTEGER NOT NULL,
+    alu_data_ini TIMESTAMP NOT NULL,
+    alu_data_final TIMESTAMP NOT NULL,
+    alu_data_ano TIMESTAMP NOT NULL,
     CONSTRAINT pk_alu_id PRIMARY KEY (alu_id),
     CONSTRAINT "TBALUGUEL -> TBCLIENTE" FOREIGN KEY (cli_id)
         REFERENCES locacao.tbcliente(cli_id),
-)
+    CONSTRAINT "TBALUGUEL -> TBCARRO" FOREIGN KEY (car_id)
+        REFERENCES locacao.tbcarro(car_id),
+);
