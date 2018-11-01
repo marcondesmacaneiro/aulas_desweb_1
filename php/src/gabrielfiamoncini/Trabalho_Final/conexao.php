8fd8c68eaeddf6d57557218c59d048eb90b9df21
Gabriel Fiamoncini <gfiamoncini@unidavi.edu.br>
2018-11-01 20:20:41 -0300

    ..

--- php/src/gabrielfiamoncini/Trabalho_Final/conexao.php
+++ php/src/gabrielfiamoncini/Trabalho_Final/conexao.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect("mysql", "root", "root", "web1");
+    $conn = mysqli_connect("mysql", "root", "root", "Trabalho");
     if (mysqli_connect_error()) {
         echo 'erro: '.mysql_connect_error();
         die();
---
+++ php/src/gabrielfiamoncini/Trabalho_Final/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" >
+    <fieldset>
+    <legend>Autenticação</legend>
+    <label >Usuário</label>
+    <input type="text" name="login" id="login">
+    <label>Senha</label>
+    <input type="password" name="senha" id="senha">
+    <input type="submit" value="logar">
+    </fieldset>
+    </form>
+</body>
+</html>
+
+
+<?php
+    include "conexao.php";
+    
+    $login = $_POST['login'];
+    $senha = $_POST['senha'];
+
+    $sql = "Select Login from Login where Login ='{$login}' and Senha='{$senha}'";
+    $result =  mysqli_query($conn,$sql);
+   
+    if (mysqli_num_rows($result) == 1) {
+        echo "Bem Vindo !";
+        include "cadastro.php";
+    } else {
+        echo'Dados Invalidos';        
+    }
+      
+
+?>
