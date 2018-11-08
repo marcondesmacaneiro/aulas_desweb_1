e6121bcaaab6d7b3df2ee813c47f2623dc698aa0
Gabriel Fiamoncini <gfiamoncini@unidavi.edu.br>
2018-11-08 19:38:58 -0200

    ..

--- php/src/gabrielfiamoncini/Trabalho_Final/conexao.php
+++ php/src/gabrielfiamoncini/Trabalho_Final/conexao.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect("127.0.0.1", "root","", "trabalho");
+    $conn = mysqli_connect("mysql", "root","root", "trabalho");
     if (mysqli_connect_error()) {
         echo 'erro: '.mysql_connect_error();
         die();
